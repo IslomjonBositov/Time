efbd7a8315dd6f1fd15f1b152759ec15c5305d2b
islomjonbositov1-ai <islomjonbosiov1@g,ail.com>
2026-04-17 18:01:49 +0500

    Add some news

--- js/index.js
+++ js/index.js
@@ -1,28 +1,13 @@
-// New Date
+let body = document.getElementsByTagName("body")[0];
 
-// const date = new Date();
-// console.log(date.getDate());
-// console.log(date.getDay());
-// console.log(date.getFullYear());
-// console.log(date.getHours());
-// console.log(date.getMilliseconds());
-// console.log(date.getMinutes());
-// console.log(date.getMonth());
-// console.log(date.getSeconds());
-// console.log(date.getTime());
-
-// console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} Shu vaqt bo'ldi. Bugun ${date.getDate()} ${date.getMonth() === 3 ? "Aprel" : "May"} va haftaning ${date.getDay() === 3 ? "Chorshanba" : "Payshanba"} kuni`);
-
-// // SetTimeout
-// setTimeout(() => {
-//   console.log("SetTimeOut");
-// }, 1000);
-
-// // SetInterval
-// setInterval(() => {
-//   console.log("SetInterval");
-// }, 5000);
-
+function lightMode() {
+  body.style.background = "linear-gradient(to top, wheat, white)";
+}
+function darkMode() {
+  body.style.background =
+  "linear-gradient(365deg, rgb(17, 17, 87), rgb(38, 64, 109)";
+}
+  
 function ShowClock() {
   const clock = document.querySelector("#clock");
   const dateRusult = new Date();
@@ -32,8 +17,7 @@ function ShowClock() {
   let seconds = dateRusult.getSeconds();
   // console.log(hours, minutes, seconds);
 
-  clock.innerHTML = `${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
-  // clock.innerHTML = `${hours > 12 ? "pm" : hours}`
+  clock.innerHTML = `${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds} ${hours < 12 ? "a.m." + hours : "p.m."}`;
   setInterval(ShowClock, 1000);
 }
 ShowClock();
@@ -43,24 +27,17 @@ function ShowInfo() {
   const time = new Date();
 
   let day = time.getDate();
-  let month = time.getMonth() + 1;
+  // let month = time.getMonth() + 1;
   let year = time.getFullYear();
 
-  // console.log(day, month, year);
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-  info.innerHTML = `${day < 10 ? "0" + day : day}. ${month < 10 ? "0" + month : month}. ${year}`;
+  let dayName = days[time.getDay()]
+  let monthName = month[time.getMonth()]
+
+  info.innerHTML = ` ${dayName}, ${day < 10 ? "0" + day : day} ${monthName}, ${year}.`;
 
   setInterval(ShowInfo, 1000);
 }
 ShowInfo();
-
-let body = document.getElementsByTagName("body")[0];
-// console.log(body);
-
-function lightMode() {
-  body.style.background = "linear-gradient(to top, wheat, white)";
-}
-function darkMode() {
-  body.style.background =
-    "linear-gradient(365deg, rgb(17, 17, 87), rgb(38, 64, 109)";
-}
